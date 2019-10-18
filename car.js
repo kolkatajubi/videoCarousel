2e704934c079e6811237c0e3d469d77bb80369de
kolkatajubi <kolkatachamps@jubi.ai>
2019-10-18 16:32:43 +0530

    first commit

---
+++ car.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  owlCarousel();
+});
+
+function owlCarousel() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+}
